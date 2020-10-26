b42f57a979588b61f8692c525463692564868054
erick silva teran <toonny1994silva@gmail.com>
2020-10-26 10:51:34 +0100

    Primer proyecto usando Git, Fotos APP

---
+++ gitFotos/gitFotos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitFotos
+//
+//  Created by erick silva teran on 26/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
