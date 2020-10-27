be8e646b5344fa902770e3189fcf22d0c0fea75a
erick silva teran <toonny1994silva@gmail.com>
2020-10-27 15:59:28 +0100

    medidas y margenes de las celdas

--- gitFotos/gitFotos/Feature/pictureCollection/PicturesViewController.swift
+++ gitFotos/gitFotos/Feature/pictureCollection/PicturesViewController.swift
@@ -9,10 +9,13 @@ import Foundation
 import UIKit
 
 class PictureViewController: UIViewController  {
-  private let reuseIdentifier = String(describing: PicturesCell.self)
-    
-    @IBOutlet weak var collectionView: UICollectionView!
-    
+  let maxHorizontalCells: CGFloat = 0
+  let marginBetweenCells: CGFloat = 5
+  
+  private let reuseIdentifier = String(describing: PicturesCell.self) //pictures cell
+  
+  @IBOutlet weak var collectionView: UICollectionView!
+  
   override func viewDidLoad() {
     // sirve para cargar el .xib en la constante
     let nib = UINib(nibName: reuseIdentifier, bundle: nil)
@@ -22,25 +25,39 @@ class PictureViewController: UIViewController  {
   }
 }
 extension PictureViewController: UICollectionViewDataSource {
-    
-    
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 150
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-        cell.backgroundColor = .black
-        return cell
-    }
-}
-  extension PictureViewController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
-      print(indexPath.item)
-    }
-extension UICollectionViewDelegateFlowLayout
-      }
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+  }
   
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 80
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    cell.backgroundColor = .black
+    return cell
+  }
+}
+
+extension PictureViewController: UICollectionViewDelegate {
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
+    print(indexPath.row)
+  }
+}
+
+extension PictureViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: 100, height: 100)
+  }
+
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return marginBetweenCells
+  }
+
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+    return maxHorizontalCells
+  }
+}
 
