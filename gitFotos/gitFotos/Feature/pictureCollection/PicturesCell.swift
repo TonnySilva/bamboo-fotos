9ba985f9d01b5a3bd8885e8eece02c0b652ec113
erick silva teran <toonny1994silva@gmail.com>
2020-10-27 13:03:09 +0100

    extension PictureViewController,

--- gitFotos/gitFotos/Feature/pictureCollection/PicturesCell.swift
+++ gitFotos/gitFotos/Feature/pictureCollection/PicturesCell.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import UIKit
+
+
+class PicturesCell: UICollectionViewCell {
+  
+  @IBOutlet weak var previewImage: UIImageView!
+  
+  @IBOutlet weak var titleLabel: UILabel!
+}
--- gitFotos/gitFotos/Feature/pictureCollection/PicturesViewController.swift
+++ gitFotos/gitFotos/Feature/pictureCollection/PicturesViewController.swift
@@ -9,23 +9,38 @@ import Foundation
 import UIKit
 
 class PictureViewController: UIViewController  {
+  private let reuseIdentifier = String(describing: PicturesCell.self)
     
     @IBOutlet weak var collectionView: UICollectionView!
     
+  override func viewDidLoad() {
+    // sirve para cargar el .xib en la constante
+    let nib = UINib(nibName: reuseIdentifier, bundle: nil)
+    collectionView.register(nib, forCellWithReuseIdentifier: reuseIdentifier)
+    super.viewDidLoad()
+    
+  }
 }
 extension PictureViewController: UICollectionViewDataSource {
     
     
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 100
+        return 150
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellIdentifier", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
         cell.backgroundColor = .black
         return cell
     }
 }
-    
+  extension PictureViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
+      print(indexPath.item)
+    }
+extension UICollectionViewDelegateFlowLayout
+      }
+  
+
