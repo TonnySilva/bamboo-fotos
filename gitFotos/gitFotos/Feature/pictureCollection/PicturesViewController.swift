fc88b3cf776c287781215412230413eb490b47a9
erick silva teran <toonny1994silva@gmail.com>
2020-10-28 13:16:52 +0100

    barra transparente de años y meses en la storyboard

--- gitFotos/gitFotos/Feature/pictureCollection/PicturesViewController.swift
+++ gitFotos/gitFotos/Feature/pictureCollection/PicturesViewController.swift
@@ -37,18 +37,18 @@ extension PictureViewController: UICollectionViewDataSource {
     
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
     if let pictureCell = (cell as? PicturesCell) {
-//      asignar un numero a cada celda
+      //      asignar un numero a cada celda
       pictureCell.titleLabel.text = "\(indexPath.row)"
-//      poner 2imagen marron - segunda opcion de poner imagen la k esta en //2
+      //      poner 2imagen marron - segunda opcion de poner imagen la k esta en //2
       pictureCell.previewImage.image = UIImage (named: "pic2")
-//      2 pictureCell.previewImage.image = #imageLiteral(resourceName: "pic2")
+      //      2 pictureCell.previewImage.image = #imageLiteral(resourceName: "pic2")
       
-//      imagen verde e imagen marron alternadas
-//      indexpathrow par o impar
-//      2solucion alternativas
-//     solucion 1
-//      pictureCell.previewImage.image = indexPath.row % 2 == 0 ? UIImage(named: "pic1") : UIImage(named: "pic2")
-//      solucion 2
+      //      imagen verde e imagen marron alternadas
+      //      indexpathrow par o impar
+      //      2solucion alternativas
+      //     solucion 1
+      //      pictureCell.previewImage.image = indexPath.row % 2 == 0 ? UIImage(named: "pic1") : UIImage(named: "pic2")
+      //      solucion 2
       if indexPath.row % 2 == 0 {
         pictureCell.previewImage.image = UIImage (named: "pic1")
         print(indexPath.row)
@@ -73,11 +73,11 @@ extension PictureViewController: UICollectionViewDelegateFlowLayout {
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     return CGSize(width: 90, height: 90)
   }
-
+  
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
     return marginBetweenCells
   }
-
+  
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
     return maxHorizontalCells
   }
