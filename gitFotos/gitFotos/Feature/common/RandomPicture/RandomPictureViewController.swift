eb9a88f077a605c00f20dcf1f6e345c8067997ab
erick silva teran <toonny1994silva@gmail.com>
2020-11-02 13:27:12 +0100

    añadido animacion al cambio de imagen en for you

--- gitFotos/gitFotos/Feature/common/RandomPicture/RandomPictureViewController.swift
+++ gitFotos/gitFotos/Feature/common/RandomPicture/RandomPictureViewController.swift
@@ -11,25 +11,55 @@ class RandomPictureViewController: UIViewController {
   
   @IBOutlet weak var imageView: UIImageView!
   
+  var timer: Timer = Timer()
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+      refreshPicture()
+      
+//     solucion 1 temporizador con cambio de imagen
+    timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(refreshPicture), userInfo: nil, repeats: true)
+      
+//      solucion 2 crear el temporizador con cambio de imagen
+    
+//      Timer.scheduledTimer(withTimeInterval: 4.0,
+//                           repeats: true,
+//                           block: { _ in
+//                            self.refreshPicture()
+//                            print("hola")
+//                          })
+//
 //      asignar una imagen aleatoria
-      let randomNumber = Int.random(in: 0 ... ImagesData.numOfImages())
       
-      let img:UIImage? = ImagesData.imageForPosition(randomNumber)
-      
-      imageView.image = img
-      
-      print("randomNumber: \(randomNumber)")
+//      let randomNumber = Int.random(in: 0 ... ImagesData.numOfImages())
+//
+//      let img:UIImage? = ImagesData.imageForPosition(randomNumber)
+//
+//      imageView.image = img
+//
+//      print("randomNumber: \(randomNumber)")
     }
   func getRandomPicture() -> UIImage? {
-    let randomNumber = Int.random(in: 0 ... ImagesData.numOfImages())
-    let img:UIImage? = ImagesData.imageForPosition(randomNumber)
+    let minPictureIndex: Int = 0
+    let maxPictureIndex: Int = ImagesData.numOfImages() - 1
+    let randomIndex = Int.random(in: minPictureIndex ... maxPictureIndex)
+    let image: UIImage? = ImagesData.imageForPosition(randomIndex)
 
-  return img
+  return image
   }
-      
-      
+  @objc func refreshPicture() {
+    imageView.image = getRandomPicture()
+    
+    
+    UIView.transition(with: imageView ,
+                      duration: 1.0,
+                      options: .transitionFlipFromTop,
+                      animations: {
+                        self.imageView.image = self.getRandomPicture()
+                      },
+                      completion: nil)
+  }
+ 
         // Do any additional setup after loading the view.
     
     
