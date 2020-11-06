4e9e97b7fbc4252e00c7c2fe90ab54c88429b8cb
erick silva teran <toonny1994silva@gmail.com>
2020-11-06 11:07:35 +0100

    Alerta en el boton reset con el boton "Si" y "No"

--- gitFotos/gitFotos/Feature/Email/EmailViewController.swift
+++ gitFotos/gitFotos/Feature/Email/EmailViewController.swift
@@ -9,29 +9,63 @@ import UIKit
 
 class EmailViewController: UIViewController {
   
+  //  primero creo la etiqueta donde guardo el string de nombre y email
+  private let nameEmailKey: String = "MyNameAndEmailKey"
+  //
+  
   @IBOutlet weak var nameText: UITextField!
   
   @IBOutlet weak var emailText: UITextField!
   
-  @IBOutlet weak var buttonGuardarOutlet: UIButton!
-  
   @IBOutlet weak var textView: UITextView!
   
+//  accion boton reset
+  @IBAction func resetButton(_ sender: UIButton) {
+    
+////    eliminar datos solucion
+//    UserDefaults.standard.removeObject(forKey: nameEmailKey)
+////    synchronize es para que lo elimine al momento de apretar el reset.
+//    UserDefaults.standard.synchronize()
+    
+//    alerta
+
+     let alert = UIAlertController(title: "CUIDADO!", message: "Estas seguro que quieres borrar?", preferredStyle: UIAlertController.Style.alert)
+    
+//accion de eliminar con el boton Si
+    let action = UIAlertAction(title: "Si", style: UIAlertAction.Style.destructive, handler:
+                                { action in
+                                  UserDefaults.standard.removeObject(forKey: self.nameEmailKey)
+                                  UserDefaults.standard.synchronize()
+                                  print("el usuario ha eliminado los datos")
+                                })
+    alert.addAction(action)
+//    no eliminar los datos con el boton NO
+    let cancelAction = UIAlertAction(title: "No", style: UIAlertAction.Style.default, handler: //nil o tmbn puedo poner prints //
+                                      {action in
+                                        print("No elimino los datos")
+                                      }
+                                     )
+    alert.addAction(cancelAction)
+    
+    
+
+      self.present(alert, animated: true, completion: nil)
+    
+    
+  }
   
   //  accion del boton
   @IBAction func guardarButtonAction(_ sender: Any) {
     
-    //
     guard let nombreTexto: String = nameText.text,
           let emailDeTexto: String = emailText.text else {return}
     
     let fullString: String = "\(nombreTexto);\(emailDeTexto)"
     UserDefaults.standard.set(fullString, forKey: nameEmailKey)
     
+   
   }
-  //  primero creo la etiqueta donde guardo el string de nombre y email
-  private let nameEmailKey: String = "MyNameAndEmailKey"
-  //
+
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -45,9 +79,13 @@ class EmailViewController: UIViewController {
     
     if let storedValue: String = UserDefaults.standard.string(forKey: nameEmailKey) {
       textView.text.append("\n" + storedValue)
+      
       print("Stored value: \(storedValue)")
       
+      
+      
     }
+  
   }
   
   /*
